a263c2e43c1c39f230db46e47d0d63da65c8e59f
shilpa-bhadra <shilpa.bhadran@gmail.com>
2021-12-18 20:33:50 +0530

    Updated TestDemoThree file

--- src/test/java/demo2/mavendemo/TestDemoThree.java
+++ src/test/java/demo2/mavendemo/TestDemoThree.java
@@ -7,5 +7,13 @@ public class TestDemoThree {
 	@Test
 	public void demoThree() {
 		System.out.println("TestDemoThree");
+		System.out.println();
+		System.out.println("TestDemoThree");
+		System.out.println();
+		System.out.println(" Person1 changes in TestDemoThree");
+		System.out.println();
+		System.out.println("NovBatch  Updates......");
+		System.out.println();
+		
 	}
 }
